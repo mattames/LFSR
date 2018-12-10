a6ba25de9e17ce3b5129f389161297e5fc8379b5
Matt Ames <Matt.ames7bc@gmail.com>
2018-12-10 14:57:46 +1100

    Initial commit of a simple LFSR implementation that is 64 bits long and uses a maximal length polynomial.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(void)
+{
+    uint64_t lfsr = 0x1234567890ABCDEFLL; //start value  
+    uint8_t cnt = 0;
+    printf("Initial Value:- %llx\n", lfsr);
+    
+    for(cnt=0;cnt<64;cnt++)
+    {
+	// Polynomial is C(x) = 1 + x^15 + x^27 + x^38 + x^46 + x^62 + x^64
+        uint64_t bit  = ((lfsr >> 63) ^ (lfsr >> 61) ^ (lfsr >> 45) ^ (lfsr >> 37) ^ (lfsr >> 26) ^ (lfsr >> 14)) & 0x1;
+        lfsr =  (lfsr << 1) | (bit);
+        printf("shift #%i, %llx\n", (cnt+1), lfsr);
+    }
+    printf("Next MI:- %llx\n", lfsr);
+    return 0;
+}
