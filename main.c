de56726229a5913c972a75d6cb53089a3b15d09b
Matt Ames <Matt.ames7bc@gmail.com>
2018-12-12 13:02:55 +1100

    Force printing of leading zeros so field always has the same indentation/formatting

--- main.c
+++ main.c
@@ -6,15 +6,15 @@ int main(void)
 {
     uint64_t lfsr = 0x1234567890ABCDEFLL; //start value  
     uint8_t cnt = 0;
-    printf("Initial Value:- %llx\n", lfsr);
+    printf("Initial Value:- %016llx\n", lfsr);
     
     for(cnt=0;cnt<64;cnt++)
     {
 	// Polynomial is C(x) = 1 + x^15 + x^27 + x^38 + x^46 + x^62 + x^64
         uint64_t bit  = ((lfsr >> 63) ^ (lfsr >> 61) ^ (lfsr >> 45) ^ (lfsr >> 37) ^ (lfsr >> 26) ^ (lfsr >> 14)) & 0x1;
         lfsr =  (lfsr << 1) | (bit);
-        printf("shift #%i, %llx\n", (cnt+1), lfsr);
+        printf("shift #%i, %016llx\n", (cnt+1), lfsr);
     }
-    printf("Next MI:- %llx\n", lfsr);
+    printf("Next MI:- %016llx\n", lfsr);
     return 0;
 }
