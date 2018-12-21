66c71127ed692ced50d80ccede8d42cbcfc790d9
Matt Ames <Matt.ames7bc@gmail.com>
2018-12-21 13:41:05 +1100

    Add in DMR LFSR calculator as well minor updates to the P25 LFSR output for clarity

---
+++ dmrlfsr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(void)
+{
+    uint32_t lfsr = 0x12345678;
+    uint8_t cnt = 0;
+    printf("Initial Value:- 0x%08x\n", lfsr);
+ 
+    for(cnt=0;cnt<32;cnt++)
+    {
+	// Polynomial is C(x) = 1 + x^2 + x^22 + x^32
+        uint32_t bit  = ((lfsr >> 31) ^ (lfsr >> 21) ^ (lfsr >> 1)) & 0x1;
+        lfsr =  (lfsr << 1) | (bit);
+        printf("shift #%i, 0x%08x\n", (cnt+1), lfsr);
+    }
+    printf("Next MI:- 0x%08x\n", lfsr);
+    return 0;
+}
+
